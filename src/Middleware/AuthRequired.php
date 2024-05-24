38f46566d1067d48e74d285afad42ca3aa10fbd0
Paul Waring <paul@xk7.net>
2024-05-24 15:22:19 +0100

    Refactor AuthMiddleware
    Changed check to be 'if logged in, continue with request, otherwise
    redirect to login'. Also used RouteContext to get a login route by name
    instead of hardcoding a path.

--- src/Middleware/AuthRequired.php
+++ src/Middleware/AuthRequired.php
@@ -9,22 +9,25 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Psr7\Response;
+use Slim\Routing\RouteContext;
 
 class AuthRequired implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         /** @todo Replace this with your authentication 'is logged in' check */
-        if (!(isset($_SESSION['logged_in']) && $_SESSION['logged_in'])) {
-            $response = new Response();
-            /** @todo Replace this with a redirect to your login route */
-            return $response->withStatus(302)->withHeader('Location', '/login');
+        if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
+            /** @todo Replace this with whatever attributes you want to pass into the request, such as the user ID */
+            $request = $request->withAttribute('user_id', $_SESSION['user_id']);
+
+            $response = $handler->handle($request);
+            return $response;
         }
 
-        /** @todo Replace this with whatever attributes you want to pass into the request, such as the user ID */
-        $request = $request->withAttribute('user_id', $_SESSION['user_id']);
+        $routeParser = RouteContext::fromRequest($request)->getRouteParser();
 
-        $response = $handler->handle($request);
-        return $response;
+        $response = new Response();
+        /** @todo Replace this with a redirect to your login route */
+        return $response->withStatus(302)->withHeader('Location', $routeParser->urlFor('login'));
     }
 }
